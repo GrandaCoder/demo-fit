dc0a9878885dd05f53c41f73d9b99cb10390f896
Granda Linux <camilo.granda@correounivalle.edu.co>
2023-02-06 07:31:50 -0500

    creacion de fragments y se fija el menu

--- app/src/main/java/com/example/demo_fit/MainActivity.kt
+++ app/src/main/java/com/example/demo_fit/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.demo_fit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.demo_fit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        //fijamos el menu principal
+        setupBottomNav()
+    }
+    private fun setupBottomNav(){
+        val fragmentManager = supportFragmentManager
+        fragmentManager.beginTransaction().add(R.id.hostFragment, HomeFragment()).commit()
     }
 }
