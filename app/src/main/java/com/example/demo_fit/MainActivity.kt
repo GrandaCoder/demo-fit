23425ab848516bf2707392a4bcfbafb0fd278e93
Granda Linux <camilo.granda@correounivalle.edu.co>
2023-02-08 08:41:21 -0500

    authentication mejoras

--- app/src/main/java/com/example/demo_fit/MainActivity.kt
+++ app/src/main/java/com/example/demo_fit/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.demo_fit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.example.demo_fit.databinding.ActivityMainBinding
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import java.util.Arrays
 
@@ -120,5 +123,20 @@ class MainActivity : AppCompatActivity() {
         super.onPause()
         // si se pausa la app, se remueve la autenticacion mientras el usuario esta en otra app o similar
         mFirebaseAuth?.removeAuthStateListener(mAuthListener)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN){
+            if(resultCode == RESULT_OK){
+                //El usuario pudo iniciar sesion or primera vez
+                Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
+            }else{
+                //esto significa que el usuario cancela la actividad de iniciar sesion
+                if(IdpResponse.fromResultIntent(data) == null){
+                    finish()
+                }
+            }
+        }
     }
 }
