836504ac2a5bface2702f2e5fd3269d82e8ff695
Granda Linux <camilo.granda@correounivalle.edu.co>
2023-05-07 12:44:10 -0500

    ya sube fotos desde el perfil, falta identificar quien lo hace

--- app/src/main/java/com/example/demo_fit/FotoPerfil.kt
+++ app/src/main/java/com/example/demo_fit/FotoPerfil.kt
@@ -4,53 +4,48 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
-import androidx.activity.result.PickVisualMediaRequest
-import androidx.activity.result.contract.ActivityResultContracts.*
-import com.example.demo_fit.databinding.ActivityFotoPerfilBinding
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
+import android.Manifest
 
 class FotoPerfil : AppCompatActivity() {
 
-    val pickMedia = registerForActivityResult(PickVisualMedia()){ uri ->
-        if(uri!=null){
-            imageSelectIv.setImageURI(uri)
-        }else{
-            Log.i("aris", "No seleccionado")
-        }
-    }
-
-    lateinit var imageSelectIv: ImageView
-    lateinit var SaveImage: Button
-    var uri: Uri? = null
-    var firebaseStorage: FirebaseStorage? = null
-    var firebaseDatabase: FirebaseDatabase? = null
-
+    private var imageSelectIv: ImageView? = null
+    private var SaveImage: Button? = null
+    private var uri: Uri? = null
+    private var firebaseDatabase: FirebaseDatabase? = null
+    private var firebaseStorage: FirebaseStorage? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_foto_perfil)
 
-        firebaseStorage = FirebaseStorage.getInstance()
-        firebaseDatabase = FirebaseDatabase.getInstance()
-
         imageSelectIv = findViewById(R.id.imageSelect_iv)
-        imageSelectIv.setOnClickListener {
-            pickMedia.launch(PickVisualMediaRequest(PickVisualMedia.ImageOnly))
+        SaveImage = findViewById(R.id.SaveImage)
+
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        firebaseStorage = FirebaseStorage.getInstance()
+
+        imageSelectIv!!.setOnClickListener {
+            uploadImage()
         }
 
-        SaveImage = findViewById(R.id.SaveImage)
-        SaveImage.setOnClickListener {
-            subirimagen()
+        SaveImage!!.setOnClickListener {
+            subirimage()
         }
 
     }
 
-    private fun subirimagen() {
+    private fun subirimage() {
         val reference = firebaseStorage!!.reference.child("Images").child(System.currentTimeMillis().toString() + "")
         reference.putFile(uri!!).addOnSuccessListener {
             reference.downloadUrl.addOnSuccessListener { uri ->
@@ -65,14 +60,30 @@ class FotoPerfil : AppCompatActivity() {
                     }
             }
         }
+    }private fun uploadImage() {
+        Dexter.withContext(this).withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+            .withListener(object : PermissionListener {
+                override fun onPermissionGranted(permissionGrantedResponse: PermissionGrantedResponse) {
+                    val intent = Intent()
+                    intent.type = "image/*"
+                    intent.action = Intent.ACTION_GET_CONTENT
+                    startActivityForResult(intent, 101)
+                }
+
+                override fun onPermissionDenied(permissionDeniedResponse: PermissionDeniedResponse) {
+                    Toast.makeText(this@FotoPerfil, "Permiso Denegado", Toast.LENGTH_SHORT).show()
+                }
+                override fun onPermissionRationaleShouldBeShown(permissionRequest: PermissionRequest, permissionToken: PermissionToken) {
+                    permissionToken.continuePermissionRequest()
+                }
+            }).check()
     }
-    /*
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 101 && resultCode == RESULT_OK) {
             uri = data!!.data
-            binding!!.ivSelector.setImageURI(uri)
+            imageSelectIv!!.setImageURI(uri)
         }
     }
-    */
 }
